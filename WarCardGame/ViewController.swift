fdf3b8a69ec38053c25eff11f0f63f81ca175083
simonjv01 <sjvargas01@gmail.com>
2021-01-17 13:47:10 -0500

    updated ViewController

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
     }
-
+    
+    
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftNumber = Int.random(in: 2...14)
+        //print(leftNumber)
+        
+        let rightNumber = Int.random(in: 2...14)
+        //print(rightNumber)
+        
+        
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+    }
+    
 
 }
 
